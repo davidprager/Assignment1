4be2ce4008bd4335fa69681e20da28930d906938
davidprager <david_prager@yahoo.com>
2022-03-10 10:29:51 -0500

    Initial commit

---
+++ areas.js
@@ -0,0 +1,27 @@
+/* This file contains functions that produce the areas of shapes
+*/
+"use strict"
+
+// define constants
+
+
+
+/* This function will display the areas of different shapes */
+function displayAreas() {
+
+}
+
+/* This function returns the area of a circle */
+function getCircleArea() {
+
+}
+
+/* This function returns the area of a square */
+function getSquareArea() {
+
+}
+
+/* This function returns the area of a triangle */
+function getTriangleArea() {
+
+}
---
+++ exchange.js
@@ -0,0 +1,28 @@
+/* This file contains functions that produce exchange rates
+*/
+
+"use strict"
+
+// Define constants
+
+
+
+/* This function will produce output showing dollars converted into different currencies */
+function displayCurrencies() {
+    // Declare exchange rate variables
+
+
+    // Perform conversions
+
+
+    // Print all of the information
+
+}
+
+/* This function exchanges money from one currency to another */
+function exchangeCurrency(){
+
+}
+
+
+
